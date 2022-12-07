5a35f797ec8a52458c32d24926cc2f8ff912f4af
abhishekpriyadarshi1998 <115804419+abhishekpriyadarshi1998@users.noreply.github.com>
2022-12-08 01:44:52 +0530

    Add files via upload

---
+++ DSA Coding Question 3.py
@@ -0,0 +1,8 @@
+# taking input from the user
+num = input('Enter a number: ')
+
+# reversing the number
+rev_num = num[::-1]
+
+# printing the reversed number
+print('The reversed number is: ', rev_num)
